1a808ccfa97cfe1ac657b4a861be3ebd599992a1
Alex Serrao <aserrao@squarespace.com>
2016-04-09 16:59:28 -0400

    Forgot I need a new event listener

--- app.js
+++ app.js
@@ -232,7 +232,7 @@ $(document).ready(function() {
 		sortTasks();
 	})
 
-	//Event listener for deleting tasks
+	//Event listeners for deleting tasks
 	$completedTasks.on('click', 'a', function(e) {
 		var confirmDelete = confirm('Are you sure  you want to delete this task? This can\'t be undone and the task will be gone forever.');
 		if(confirmDelete == true) {
@@ -242,9 +242,18 @@ $(document).ready(function() {
 			thisTaskRef.child(thisTaskID).remove();
 			sortTasks();
 		}
+	})
+	
+	$inProgressTasks.on('click', 'a', function(e) {
+		var confirmDelete = confirm('Are you sure  you want to delete this task? This can\'t be undone and the task will be gone forever.');
+		if(confirmDelete == true) {
+			var uid = firebase.getAuth().uid,
+				thisTaskID = $(this).data('deletion'),
+				thisTaskRef = firebase.child('users').child(uid).child('task');
+			thisTaskRef.child(thisTaskID).remove();
+			sortTasks();
+		}
 	})	
-
-
 
 
 
