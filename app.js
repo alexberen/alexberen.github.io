67a62581252c145229556083940c8efb5d068599
Alex Serrao <aserrao@squarespace.com>
2016-04-09 17:22:47 -0400

    Testing modal for deleting completed tasks

--- app.js
+++ app.js
@@ -234,7 +234,17 @@ $(document).ready(function() {
 
 	//Event listeners for deleting tasks
 	$completedTasks.on('click', 'a', function(e) {
-		var confirmDelete = confirm('Are you sure  you want to delete this task? This can\'t be undone and the task will be gone forever.');
+		// var confirmDelete = confirm('Are you sure  you want to delete this task? This can\'t be undone and the task will be gone forever.');
+		var confirmDelete = false;
+
+		modal.open({
+			content: $deleteConfirmationContent
+		})
+
+		$('#deleteTask').on('click', function(e) {
+			confirmDelete = true;
+		})
+
 		if(confirmDelete == true) {
 			var uid = firebase.getAuth().uid,
 				thisTaskID = $(this).data('deletion'),
@@ -253,14 +263,7 @@ $(document).ready(function() {
 			thisTaskRef.child(thisTaskID).remove();
 			sortTasks();
 		}
-	})	
-
-	// Initializing modal for confirming deletion
-	// $('#createNewTask').on('click', function(e) {
-	// 	modal.open({
-	// 		content: $formContent
-	// 	});
-	// });
+	})
 
 
 
