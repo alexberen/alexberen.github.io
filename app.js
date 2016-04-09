8dd6a2a212b4ecceea78c9c4d3e682215a3e633d
Alex Serrao <aserrao@squarespace.com>
2016-04-09 15:53:59 -0400

    Attempting to put the add tasks form in a modal

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ $(document).ready(function() {
 		$logInButton = $('#logInButton'),
 		$logOut = $('#logOut'),
 		$showCompletedChevron = $('#showCompletedChevron'),
+		$createNewTask = $('#createNewTask'),
 		authData = firebase.getAuth();
 
 	// Handlebars variables
@@ -121,6 +122,60 @@ $(document).ready(function() {
 		}
 
 	})
+
+	// Modal Test for making new Tasks
+	var modal = function() {
+		var $window = $(window),
+			$modal = $('<div class="modal" />'),
+			$content = $('div class="modal-content" />'),
+			$close = $('<button role="button" class="modal-close">X</button>');
+
+		$modal.append($content, $close);
+
+		$close.on('click', function(e) {
+			e.preventDefault();
+
+			modal.close();
+		})
+
+		return {
+			center: function() {
+				var top = Math.max($window.height() - $modal.outerHeight(), 0) / 2,
+					left = Math.max(window.width() - $modal.outerWidth(), 0) / 2;
+				$modal.css({
+					top: top + $window.scrollTop(),
+					left: left + $window.scrollLeft()
+				});
+			},
+			open: function(settings) {
+				$content.empty().append(settings.content);
+
+				$modal.css({
+					width: settings.width || 'auto',
+					height: settings.height || 'auto'
+				}).appendTo('body');
+
+				modal.center();
+				$(window).on('resize', modla.center);
+			},
+			close: function() {
+				$content.empty();
+				$modal.detach();
+				$(window).off('resize', modal.center);
+			}
+		};
+	}();
+
+	// Initializing Modal for adding new tasks
+	(function() {
+		var $content = $('#addTaskForm').detach();
+
+		$('#createNewTask').on('click', function(e) {
+			modal.open({
+				content: $content
+			});
+		});
+	}());
 	
 	// Creating tasks
 	$addTaskForm.submit(function(e) {
