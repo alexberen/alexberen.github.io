11a56947e3a35941dbe9175d425d229f402ec89f
Alex Serrao <aserrao@squarespace.com>
2016-04-10 16:15:09 -0400

    Console log

--- app.js
+++ app.js
@@ -326,9 +326,9 @@ $(document).ready(function() {
 			$newCategory = $('#newCategory'),
 			uid = firebase.getAuth().uid,
 			thisTaskID = $(this).data('edit'),
-			thisTaskRef = firebase.child('users').child(uid).child('task').child(thisTaskID);
+			thisTaskRef = firebase.child('users').child(uid).child('task');
 
-		console.log(thisTaskRef.child(thisTaskID).taskName);
+		console.log(thisTaskRef);
 		// var context = {
 		// 	editName: thisTaskRef.taskName,
 		// 	editCategory: thisTaskRef.taskCategory,
